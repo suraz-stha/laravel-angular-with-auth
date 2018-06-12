063274397c81910448f42f17898aee4a13dffbf9
stha-suraz <suraz.lazyboy@gmail.com>
2018-06-12 16:21:55 +0545

    jwt config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SignUpRequest;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','signup']]);
     }
 
     /**
@@ -27,10 +29,14 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Email Or Password does\'t exist' ], 401);
         }
 
         return $this->respondWithToken($token);
+    }
+    public function signup(SignUpRequest $request){
+        User::create($request->all());
+        return $this->login($request);
     }
 
     /**
